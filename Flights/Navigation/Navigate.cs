ba739d244d65d78e890aa1136b6e6ab2e4da8f41
“kVadm” <1408vk@gmail.com>
2017-01-14 14:45:23 +0200

    E2E start

---
+++ Flights/Navigation/Navigate.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Automation;
+using TestStack.White;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.TabItems;
+
+namespace Flights.Navigation
+{
+    public static class Navigate
+    {
+        public static void login(){
+
+            var FlightsLoginDialog = Desktop.Instance.Windows().First(w => w.Name.Contains("HPE MyFlight Sample"));
+            var textBox_Name = FlightsLoginDialog.Get<TextBox>(SearchCriteria.ByAutomationId("agentName"));
+            var textBox_Password = FlightsLoginDialog.Get<TextBox>(SearchCriteria.ByAutomationId("password"));
+            var btn_OK = FlightsLoginDialog.Get<Button>(SearchCriteria.ByAutomationId("okButton"));
+
+            textBox_Name.SetValue("john");
+            textBox_Password.SetValue("hp");
+            btn_OK.Click();
+        }
+
+        public static void OpenSearchTab()
+        {
+            login();
+            var FlightsMainWindow = Desktop.Instance.Windows().First(w => w.Name.Contains("HPE MyFlight Sample"));
+            var usernameTitle = FlightsMainWindow.Get(SearchCriteria.ByAutomationId("usernameTitle"));
+            var tabs = FlightsMainWindow.Get<Tab>(SearchCriteria.ByControlType(ControlType.Tab));
+            tabs.SelectTabPage("SEARCH ORDER");
+        }
+
+    }
+}
--- Flights/Tests.cs
+++ Flights/Tests.cs
@@ -12,17 +12,22 @@ using TestStack.White.UIItems;
 using TestStack.White.UIItems.Finders;
 using TestStack.White.UIItems.TabItems;
 using TestStack.White.UIItems.WindowItems;
+using Flights.Navigation;
+using TestStack.White.UIItems.ListBoxItems;
 
 namespace Flights
 {
     [TestFixture]
     public class Tests
-    {
+    {       
+
+
         [Test, Sequential]        
         public void loginCheck(
                                 [Values("blabla", "john",   "blabla", "john")]string name,
                                 [Values("blabla", "blabla", "hp",     "hp"  )]string password,
                                 [Values(false,     false,    false,    true )]bool   positiveTest
+                               // [Values(ErrorMsg.onlyNubmers,  ErrorMsg.notExist, ErrorMsg.lessThanZero)]string expectedMessage
                                 )
         {
             string expectedValueNegative = "Login Failed";
@@ -58,56 +63,104 @@ namespace Flights
         [Test]
         public void searchButtonAvailability()
         {
-            string expectedValuePositive = "John Smith";
-            var FlightsLoginDialog = Desktop.Instance.Windows().First(w => w.Name.Contains("HPE MyFlight Sample"));
-            var textBox_Name = FlightsLoginDialog.Get<TextBox>(SearchCriteria.ByAutomationId("agentName"));
-            var textBox_Password = FlightsLoginDialog.Get<TextBox>(SearchCriteria.ByAutomationId("password"));
-            var btn_OK = FlightsLoginDialog.Get<Button>(SearchCriteria.ByAutomationId("okButton"));
-
-            textBox_Name.SetValue("john");
-            textBox_Password.SetValue("hp");
-            btn_OK.Click();
+            Navigate.OpenSearchTab();
 
             var FlightsMainWindow = Desktop.Instance.Windows().First(w => w.Name.Contains("HPE MyFlight Sample"));
-            var usernameTitle = FlightsMainWindow.Get(SearchCriteria.ByAutomationId("usernameTitle"));
-
-            Assert.IsTrue(expectedValuePositive == usernameTitle.Name);
-
-            var tabs = FlightsMainWindow.Get<Tab>(SearchCriteria.ByControlType(ControlType.Tab));
-            tabs.SelectTabPage("SEARCH ORDER");
-
             var radioBtn_orderNumber = FlightsMainWindow.Get<RadioButton>(SearchCriteria.ByAutomationId("byNumberRadio"));
-            radioBtn_orderNumber.Click();
-
             var btn_SEARCH = FlightsMainWindow.Get<Button>(SearchCriteria.ByAutomationId("searchBtn"));
+
+            radioBtn_orderNumber.Click();
             Assert.IsTrue(!btn_SEARCH.Enabled);
         }
 
         [Test]
         public void orderNumberAvailability()
         {
-            string expectedValuePositive = "John Smith";
-            var FlightsLoginDialog = Desktop.Instance.Windows().First(w => w.Name.Contains("HPE MyFlight Sample"));
-            var textBox_Name = FlightsLoginDialog.Get<TextBox>(SearchCriteria.ByAutomationId("agentName"));
-            var textBox_Password = FlightsLoginDialog.Get<TextBox>(SearchCriteria.ByAutomationId("password"));
-            var btn_OK = FlightsLoginDialog.Get<Button>(SearchCriteria.ByAutomationId("okButton"));
-
-            textBox_Name.SetValue("john");
-            textBox_Password.SetValue("hp");
-            btn_OK.Click();
+            Navigate.OpenSearchTab();
 
             var FlightsMainWindow = Desktop.Instance.Windows().First(w => w.Name.Contains("HPE MyFlight Sample"));
-            var usernameTitle = FlightsMainWindow.Get(SearchCriteria.ByAutomationId("usernameTitle"));
-
-            Assert.IsTrue(expectedValuePositive == usernameTitle.Name);
-
-            var tabs = FlightsMainWindow.Get<Tab>(SearchCriteria.ByControlType(ControlType.Tab));
-            tabs.SelectTabPage("SEARCH ORDER");
-
             var textBox_orderNumber = FlightsMainWindow.Get<TextBox>(SearchCriteria.ByAutomationId("byNumberWatermark"));
             Assert.IsTrue(!textBox_orderNumber.Enabled);
         }
-   
+
+
+        [Test, Sequential]
+        public void incorrectOrderNumber(
+                                        [Values("Some text",           "3333333",        "-5555"               )]string orderNumber,
+                                        [Values(ErrorMsg.onlyNubmers,  ErrorMsg.notExist, ErrorMsg.lessThanZero)]string expectedMessage
+                                        )
+        {
+            Navigate.OpenSearchTab();
+
+            var FlightsMainWindow = Desktop.Instance.Windows().First(w => w.Name.Contains("HPE MyFlight Sample"));
+            var radioBtn_orderNumber = FlightsMainWindow.Get<RadioButton>(SearchCriteria.ByAutomationId("byNumberRadio"));
+            var textBox_orderNumber = FlightsMainWindow.Get<TextBox>(SearchCriteria.ByAutomationId("byNumberWatermark"));
+            var btn_SEARCH = FlightsMainWindow.Get<Button>(SearchCriteria.ByAutomationId("searchBtn"));
+
+            radioBtn_orderNumber.Click();
+            textBox_orderNumber.SetValue(orderNumber);
+            btn_SEARCH.Click();
+
+            var ErrorDialog = FlightsMainWindow.ModalWindow(SearchCriteria.ByControlType(ControlType.Window));
+            var LoginFailed_btn_OK = ErrorDialog.Get<Button>(SearchCriteria.ByControlType(ControlType.Button));
+            var ErrorMassage = ErrorDialog.Get<Label>(SearchCriteria.ByClassName("Static"));
+
+            Assert.IsTrue(expectedMessage == ErrorMassage.Name);
+            Thread.Sleep(1000);  // Pause to see 
+            LoginFailed_btn_OK.Click();
+        }
+
+
+        [Test]
+        public void E2E()
+        {
+            Navigate.login();
+            var FlightsMainWindow = Desktop.Instance.Windows().First(w => w.Name.Contains("HPE MyFlight Sample"));
+            var cmb_fromCity = FlightsMainWindow.Get<ComboBox>(SearchCriteria.ByAutomationId("fromCity"));
+            var cmb_toCity = FlightsMainWindow.Get<ComboBox>(SearchCriteria.ByAutomationId("toCity"));
+            var cmb_class = FlightsMainWindow.Get<ComboBox>(SearchCriteria.ByAutomationId("Class"));
+            var cmb_numOfTickets = FlightsMainWindow.Get<ComboBox>(SearchCriteria.ByAutomationId("numOfTickets"));
+            var textBox_Date = FlightsMainWindow.Get(SearchCriteria.ByAutomationId("datePicker"));
+            var btn_findFlights = FlightsMainWindow.Get<Button>(SearchCriteria.ByAutomationId(""));
+
+
+            cmb_fromCity.Click();         
+            foreach (var city in cmb_fromCity.Items)
+            {
+                if (city.Name == "Frankfurt") { city.Click(); }
+            }
+            Thread.Sleep(1000);  // Pause to see 
+
+            cmb_toCity.Click();
+            Thread.Sleep(1000);  // Pause to see 
+            foreach (var city in cmb_toCity.Items)
+            {
+                if (city.Name == "Sydney") { city.Click(); }
+            }
+            
+
+            textBox_Date.Click();
+            textBox_Date.SetValue(TestStack.White.WindowsAPI.KeyboardInput.SpecialKeys.CONTROL+"A");
+            textBox_Date.SetValue(TestStack.White.WindowsAPI.KeyboardInput.SpecialKeys.BACKSPACE);
+
+            cmb_class.Click();
+            Thread.Sleep(1000);  // Pause to see
+            foreach (var item in cmb_class.Items)
+            {
+                if (item.Name == "First") { item.Click(); }
+            }
+            
+
+            cmb_numOfTickets.Click();
+            Thread.Sleep(1000);  // Pause to see
+            foreach (var item in cmb_numOfTickets.Items)
+            {
+                if (item.Name == "3") { item.Click(); }
+            }
+           
+
+            btn_findFlights.Click();
+        }
 
 
 
@@ -135,7 +188,14 @@ namespace Flights
     {
         public const string PATH = "D:\\ADM Automation\\ADM exam task\\ADM exam task GUI\\Flights Application\\FlightsGUI.exe";
         public const string ProcessName = "FlightsGUI";
+    }
 
+    public static class ErrorMsg
+    {
+        public const string onlyNubmers = "Enter a number.";
+        public const string notExist = "Order number does not exist.";
+        public const string lessThanZero = "Enter a number greater than 0.";
+        
     }
 }
 
